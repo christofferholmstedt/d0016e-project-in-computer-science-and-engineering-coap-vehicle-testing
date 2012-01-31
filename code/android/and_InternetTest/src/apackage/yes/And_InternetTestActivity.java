6a4ff1d380ae4e8b9632fcc6909fff20fc8c6320
Gurr3 <gurr72@gmail.com>
2012-01-31 13:26:22 +0100

    commit av okänt innehåll

--- code/android/and_InternetTest/src/apackage/yes/And_InternetTestActivity.java
+++ code/android/and_InternetTest/src/apackage/yes/And_InternetTestActivity.java
@@ -3,6 +3,7 @@ package apackage.yes;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -27,7 +28,7 @@ public class And_InternetTestActivity extends Activity {
 			System.out.println("connect() failed");
 			e.printStackTrace();
 			// TODO: handle exception
-		}
+		}	
 	}
 
 	public void Connect(String ipAdress) throws Exception {
@@ -47,73 +48,9 @@ public class And_InternetTestActivity extends Activity {
 				dest, 15900);
 		System.out.println("packet förberedd");
 
-		int x = 0;
-		while (x < 100) {
-			x++;
-			socket.send(packet);
-			System.out.println("skickat");
-		}
+		socket.send(packet);
+		System.out.println("skickat");
+
 	}
 
 }
-
-// WIFI MANAGER !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// w = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
-//
-// if (!w.isWifiEnabled()) {
-// textview.append("switching ON wifi ");
-// w.setWifiEnabled(true);
-// } else textview.setText("Its already ON ");
-//
-// WifiInfo info = w.getConnectionInfo();
-// textview.append("\n\nWiFi Status: " + info.toString());
-
-// int x = info.getIpAddress();
-
-// String macA = info.getMacAddress();
-// textview.append("\n\nmac address===" + macA + "  ,ip===" + x);
-
-// try {
-// server_ip = InetAddress.getByName(ipaddress); //translates string to
-// inetaddress
-// } catch (UnknownHostException e) {
-// System.out.println("Error: fetching inetAddress");
-// textview.append("Error at fetching inetAddress");
-// e.printStackTrace();
-// }
-//
-//
-// try {
-// // DSocket = new DatagramSocket(server_port, server_ip);
-// DSocket = new DatagramSocket();
-// } catch (SocketException e) {
-// System.out.println("Error: Creating Dsocket");
-// textview.append("Error at creating Datagramsocket");
-// e.printStackTrace();
-// }
-//
-// byte b1[] = message.getBytes();
-// DatagramPacket p1 = new DatagramPacket(b1, b1.length, server_ip,
-// server_port);
-//
-// try {
-// DSocket.send(p1);
-// textview.append("Sent message");
-// System.out.println("Sent message");
-// } catch (IOException e) {
-// System.out.println("Error: sending packet");
-// textview.append("Error at sending packet");
-// e.printStackTrace();
-// }
-//
-// DatagramPacket p2 = new DatagramPacket(null, 100);
-// try {
-// DSocket.receive(p2);
-// textview.append("message recieved: " + p2.toString());
-// System.out.println("message recieved: "+ p2.toString());
-// } catch (IOException e) {
-// System.out.println("Error: recieve message");
-// textview.append("Error at recieve message");
-// e.printStackTrace();
-// }
-
